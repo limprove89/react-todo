e1972f26d35663a1c6dce25eebc6aa998007dd50
원영선 <ifyousun@won-yeongseon-ui-MacBookPro.local>
2020-02-24 13:10:53 +0900

    final!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {createGlobalStyle} from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 import TodoTemplate from './components/TodoTemplate';
-import TodoHeader from './components/TodoHeader';
+import TodoHead from './components/TodoHead';
 import TodoList from './components/TodoList';
 import TodoCreate from './components/TodoCreate';
 import { TodoProvider } from './components/TodoContext';
@@ -15,12 +15,12 @@ const GlobalStyle = createGlobalStyle`
 function App() {
   return (
     <TodoProvider>
-    <GlobalStyle />
-    <TodoTemplate>
-        <TodoHeader />
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
         <TodoList />
         <TodoCreate />
-    </TodoTemplate>
+      </TodoTemplate>
     </TodoProvider>
   );
 }
